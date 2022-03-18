d27ed0644e1a15d6ea595469a60f39c545d58d92
Zitrong <786019674@qq.com>
2022-03-18 17:28:00 +0800

    read to a dictionary as key

--- RelayTable/RelayTable.py
+++ RelayTable/RelayTable.py
@@ -1,30 +1,45 @@
 import xlwings as xw
-
 import numpy as np
 import pandas as pd
 
 
+
 def main():
 
+
     wb = xw.Book.caller()
+
     Read_in = wb.sheets[0]
+
     Write = wb.sheets[1]
+
     table = Read_in.range('A1','H15').value
-    
+    relaytable = {}
+    head = table[0][1:]
+    for r,row in enumerate(table[1:]):
+        for c,model in enumerate(row[1:]):
+            relaytable[head[c]+"->"+model]=""
+    print(relaytable)
+
 
 
 
 
 @xw.func
 
+
 def hello(name):
+
 
     return f"Hello {name}!"
 
 
 
+
 if __name__ == "__main__":
+
 
     xw.Book("RelayTable.xlsm").set_mock_caller()
     main()
+
 
