6cfbaf2f1af2b65a5fb78775b43599d997ea894c
Zitrong <786019674@qq.com>
2022-03-18 18:09:15 +0800

    add number

--- RelayTable/RelayTable.py
+++ RelayTable/RelayTable.py
@@ -16,14 +16,17 @@ def main():
     table = Read_in.range('A1','H15').value
     relaytable = {}
     head = table[0][1:]
+    relay_count = 0
     for r,row in enumerate(table[1:]):
         for c,model in enumerate(row[1:]):
-            head[c].lstrip()
-            head[c].rstrip()
-            head[c] = head[c].replace(" ","")
-            relaytable[head[c]+"->"+model]=""
+            line = head[c]
+            line.lstrip()
+            line.rstrip()
+            line = line.replace(" ","")
+            if not(relaytable.__contains__(line)):
+                relaytable[head[c]+"->"+model]=relay_count
+                relay_count = relay_count + 1
     print(relaytable)
-
 
 
 
