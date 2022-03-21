6d1eee4436d6f2e1c17bbc37a43824381742650f
Zitrong <786019674@qq.com>
2022-03-21 09:12:13 +0800

    fix

--- RelayTable/RelayTable.py
+++ RelayTable/RelayTable.py
@@ -1,5 +1,3 @@
-import xlwings as xw
-import numpy as np
 import pandas as pd
 
 
@@ -7,31 +5,27 @@ import pandas as pd
 def main():
 
 
-    wb = xw.Book.caller()
+    fileNameStr = "RelayTable/RelayTable.xlsx"
+    xl = pd.ExcelFile(fileNameStr)
+    table = xl.parse('Sheet1')
 
-    Read_in = wb.sheets[0]
-
-    Write = wb.sheets[1]
-
-    table = Read_in.range('A1','H15').value
     relaytable = {}
-    head = table[0][1:]
+    head = table.values[0][1:]
     relay_count = 0
-    for r,row in enumerate(table[1:]):
+
+    for r,row in enumerate(table.values[1:]):
         for c,model in enumerate(row[1:]):
-            line = head[c]
+            line = str(head[c])+"->"+str(model)
             line.lstrip()
             line.rstrip()
             line = line.replace(" ","")
             if not(relaytable.__contains__(line)):
-                relaytable[head[c]+"->"+model]=relay_count
+                relaytable[line]=relay_count
                 relay_count = relay_count + 1
     print(relaytable)
 
 
 
-
-@xw.func
 
 
 def hello(name):
@@ -43,9 +37,6 @@ def hello(name):
 
 
 if __name__ == "__main__":
-
-
-    xw.Book("RelayTable.xlsm").set_mock_caller()
     main()
 
 
