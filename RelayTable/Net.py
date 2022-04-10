bc11424bb8cf05fc36e7762879bdb04c18f88cc4
Zitrong Wu <786019674@qq.com>
2022-04-10 14:19:27 +0800

    Net as a basic class refferenced by Pin

--- RelayTable/Net.py
+++ RelayTable/Net.py
@@ -1,4 +1,3 @@
-from Pin import Pin
 class Net:
     count = -1
     def __init__(self,name = "") -> None:
@@ -8,19 +7,5 @@ class Net:
             self.name = f'K{self.number}'
         else:
             self.name = name    
-        self.pins = list()    
-    def connect(self,pin:Pin):
-        self.pins.append(pin)
 
 
-def main():
-    pin1 = Pin("Pin1")
-    pin2 = Pin("Pin2")
-    net = Net("defult")
-    net.connect(pin1)
-    net.connect(pin2)
-    print(net.pins[0].name)
-    print(net.pins[1].name)
-
-if __name__ == "__main__":
-    main()
--- RelayTable/Pin.py
+++ RelayTable/Pin.py
@@ -1,8 +1,10 @@
+from Net import Net
 class Pin:
     count = -1
     def __init__(self, name=""):
         Pin.count +=1
         self.number = Pin.count
+        self.Net = Net("defult")
         if name == "":
             self.name = f'Net{self.number}'
         else:
