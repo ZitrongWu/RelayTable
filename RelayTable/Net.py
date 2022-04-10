308a131bc63df1d01ce09f8954ea9cd0dd02ba6c
Zitrong Wu <786019674@qq.com>
2022-04-10 15:32:07 +0800

    complete pin and net

--- RelayTable/Net.py
+++ RelayTable/Net.py
@@ -3,6 +3,7 @@ class Net:
     def __init__(self,name = "") -> None:
         Net.count +=1
         self.number = Net.count
+        self.pins = list()
         if name == "":
             self.name = f'K{self.number}'
         else:
--- RelayTable/Pin.py
+++ RelayTable/Pin.py
@@ -4,8 +4,30 @@ class Pin:
     def __init__(self, name=""):
         Pin.count +=1
         self.number = Pin.count
-        self.Net = Net("defult")
         if name == "":
             self.name = f'Net{self.number}'
         else:
             self.name = name
+    def __str__(self):
+        return f'Pin{self.number},name:{self.name},net:{self.net.name}'
+def connect(pinself:Pin,pin:Pin,netname = ""):
+    if netname == "":
+        pinself.net = Net() 
+    else:
+        pinself.net = Net(netname)
+    pinself.net.pins.append(pinself)
+    pinself.net.pins.append(pin)
+    pin.net = pinself.net
+
+def main():
+    pin1 = Pin("pin1")
+    pin2 = Pin("pin2")
+    connect(pin1,pin2,"new net")
+    print("Test")
+    print(pin1)
+    print(pin2)
+    print(pin1.net.pins)
+   
+if __name__ == '__main__':
+    main()
+
