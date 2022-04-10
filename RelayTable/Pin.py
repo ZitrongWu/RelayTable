340284a50dba6c477d6970e1c1a8d56342035b88
ZicongWu <786019674@qq.com>
2022-04-10 21:21:21 +0800

    instanalize net in pin class

--- RelayTable/Pin.py
+++ RelayTable/Pin.py
@@ -1,30 +1,45 @@
-from Net import Net
+
+class Net:
+    count = -1
+    def __init__(self,name = "") -> None:
+        Net.count +=1
+        self.number = Net.count
+        self.pins = list()
+        if name == "":
+            self.name = f'K{self.number}'
+        else:
+            self.name = name    
+
+
 class Pin:
     count = -1
     def __init__(self, name=""):
         Pin.count +=1
         self.number = Pin.count
+        self.net = Net(f'NetP{self.number}')
         if name == "":
-            self.name = f'Net{self.number}'
+            self.name = f'Pin{self.number}'
         else:
             self.name = name
     def __repr__(self):
         return f'Pin{self.number}_{self.name}_{self.net.name}'
+
 def connect(pinself:Pin,pin:Pin,netname = ""):
-    if netname == "":
-        pinself.net = Net() 
-    else:
-        pinself.net = Net(netname)
+    if netname != "":
+        pinself.net.name = netname
+    pin.net = pinself.net
     pinself.net.pins.append(pinself)
     pinself.net.pins.append(pin)
-    pin.net = pinself.net
+    
 
-# def main():
-#     pin1 = Pin("pin1")
-#     pin2 = Pin("pin2")
-#     connect(pin1,pin2,"newnet")
-#     print(pin1.net.pins)
+
+
+def main():
+    pin1 = Pin()
+    pin2 = Pin()
+    connect(pin1,pin2,"newnet")
+    print(pin1.net.pins)
    
-# if __name__ == '__main__':
-#     main()
+if __name__ == '__main__':
+    main()
 
